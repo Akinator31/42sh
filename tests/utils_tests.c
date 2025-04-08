04ca5dfb92bd675d21066141db045d30c6a4d015
Kevin SALANIER <kevin.salanier@epitech.eu>
2025-04-08 15:13:11 +0200

    tests(semicolons): Add a basic unit tests on semicolons

--- tests/utils_tests.c
+++ tests/utils_tests.c
@@ -8,3 +8,12 @@
 #include <criterion/criterion.h>
 #include "utils.h"
 
+extern char **environ;
+
+Test(handle_semicolons, basic_handle_semicolons)
+{
+    int error_code = 0;
+    int return_value = handle_semicolons("ls ; echo abc", &environ, &error_code);
+
+    cr_assert_eq(return_value, 1);
+}
