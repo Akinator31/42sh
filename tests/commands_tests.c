3046268cda8fe0de77255e6faada99207836e423
Nogo4 <noah.gomes@epitech.eu>
2025-04-08 11:28:43 +0200

    refactor(commands_tests.c): add unit test for cd and env

--- tests/commands_tests.c
+++ tests/commands_tests.c
@@ -5,6 +5,64 @@
 ** commands_tests
 */
 
+#include <unistd.h>
 #include <criterion/criterion.h>
+#include <criterion/redirect.h>
+#include "mysh.h"
+#include "utils.h"
 #include "commands.h"
+#include "my_lib.h"
 
+extern char **environ;
+
+Test(analyse_command, basic_analyse_command)
+{
+    char **env = duplicate_2d_char_array(environ, get_2d_arr_len(environ));
+    int error_code = 1;
+
+    cr_assert_eq(analyse_command(&env, "/bin/ls", &error_code), NORMAL);
+}
+
+Test(analyse_command_, basic_analyse_command_pipe)
+{
+    char **env = duplicate_2d_char_array(environ, get_2d_arr_len(environ));
+    int error_code = 1;
+
+    cr_assert_eq(analyse_command(&env, "ls | cat -e", &error_code), NORMAL);
+}
+
+Test(is_cd_command, is_cd_command_false)
+{
+    char **env = duplicate_2d_char_array(environ, get_2d_arr_len(environ));
+    int error_code = 1;
+    exit_status_t *status;
+
+    cr_assert_eq(is_cd_command(&env, "ls", status, &error_code), false);
+}
+
+Test(is_cd_command, is_cd_command_true)
+{
+    char **env = duplicate_2d_char_array(environ, get_2d_arr_len(environ));
+    int error_code = 1;
+    exit_status_t *status;
+
+    cr_assert_eq(is_cd_command(&env, "cd", status, &error_code), true);
+}
+
+Test(is_env_command, is_env_command_false)
+{
+    char **env = duplicate_2d_char_array(environ, get_2d_arr_len(environ));
+    int error_code = 1;
+    exit_status_t *status;
+
+    cr_assert_eq(is_env_command(&env, "ls", status, &error_code), false);
+}
+
+Test(is_env_command, is_env_command_true)
+{
+    char **env = duplicate_2d_char_array(environ, get_2d_arr_len(environ));
+    int error_code = 1;
+    exit_status_t *status;
+
+    cr_assert_eq(is_env_command(&env, "env", status, &error_code), true);
+}
