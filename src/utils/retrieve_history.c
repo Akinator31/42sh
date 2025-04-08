1795e4afeb7b9939efff0bddf03d0077d4226437
Kevin SALANIER <kevin.salanier@epitech.eu>
2025-04-08 17:17:40 +0200

    refactor(retrive_history): Move function into a other file to reused it

---
+++ src/utils/retrieve_history.c
@@ -0,0 +1,24 @@
+/*
+** EPITECH PROJECT, 2025
+** B-PSU-200-TLS-2-1-minishell1-pavel.de-wavrechin
+** File description:
+** retrieve_history
+*/
+
+#include "utils.h"
+#include <stdlib.h>
+#include <string.h>
+
+char *retrive_history_path(char ***envp)
+{
+    char *homepath = get_environ_variable_value(envp, "HOME");
+    char *logpath = NULL;
+
+    if (!homepath)
+        homepath = "/tmp/";
+    logpath = malloc(sizeof(char) * strlen(homepath) + 16);
+    logpath = strcpy(logpath, homepath);
+    free(homepath);
+    logpath = strcat(logpath, "/.42sh_history");
+    return logpath ? logpath : NULL;
+}
