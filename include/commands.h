df5b5bd19da56bf6e19c6e9c549de5b452042b94
Kevin SALANIER <kevin.salanier@epitech.eu>
2025-04-08 15:17:32 +0200

    refactor(commands.h): Add function prototype to new history command

--- include/commands.h
+++ include/commands.h
@@ -24,5 +24,9 @@ bool is_cd_command(char ***envp, char *command,
     exit_status_t *status, int *error_code);
 void my_exec(char ***envp, char *command,
     exit_status_t *status, int *error_code);
+bool is_history_command(char ***envp, char *command,
+    exit_status_t *status, int *error_code);
+
+void my_write_history(char *line, char ***envp);
 
 #endif
