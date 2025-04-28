a247d18c5cb6382c775e62a21612c484e707e305
Nogo4 <noah.gomes@epitech.eu>
2025-04-28 23:36:56 +0200

    refactor(handle_command.c, get_binary.c): improve memory management and command handling

--- src/utils/handle_command.c
+++ src/utils/handle_command.c
@@ -30,30 +30,33 @@ int search_inhib(char *command)
 
 static void free_all_array(char **arr1, char **arr2)
 {
-    free_2d_array_of_char(arr1);
-    free_2d_array_of_char(arr2);
+    if (arr1)
+        free_2d_array_of_char(arr1);
+    if (arr2)
+        free_2d_array_of_char(arr2);
 }
 
 static int find_alias(char *command, alias_t *alias)
 {
     int i = 0;
     int nb_alias = alias->nb_alias;
-    char **command_array = str_to_word_array(command, " \t");
+    char **command_array = str_to_word_array(command, " \t\n");
     char **alias_array = NULL;
 
     if (!command_array)
         return -1;
     while (i != nb_alias) {
-        alias_array = str_to_word_array(alias[i].alias_name, " \t");
+        alias_array = str_to_word_array(alias[i].alias_name, " \t\n");
         if (!alias_array)
             return -1;
         if (strcmp(command_array[0], alias_array[0]) == 0) {
             free_all_array(command_array, alias_array);
             return i;
         }
+        free_2d_array_of_char(alias_array);
         i++;
     }
-    free_all_array(command_array, alias_array);
+    free_2d_array_of_char(command_array);
     return -1;
 }
 
@@ -61,7 +64,7 @@ static char *set_real_command(char *command, config_rc_t *config,
     int alias_index)
 {
     char *real_command = NULL;
-    char **command_array = str_to_word_array(command, " \t");
+    char **command_array = str_to_word_array(command, " \t\n");
 
     if (get_2d_arr_len(command_array) == 1) {
         free_2d_array_of_char(command_array);
@@ -72,17 +75,28 @@ static char *set_real_command(char *command, config_rc_t *config,
     return real_command;
 }
 
-static char **format_command(char *command, config_rc_t *config)
+static void *handle_alias(char *real_command, char *command, sh_t *sh_st,
+    int alias_index)
+{
+    real_command = set_real_command(command, sh_st->config, alias_index);
+    sh_st->config->is_alias_call = true;
+    analyse_command(sh_st->envp, real_command, sh_st->error_code,
+        sh_st->config);
+    sh_st->config->is_alias_call = false;
+    return NULL;
+}
+
+static char **format_command(char *command, config_rc_t *config, sh_t *sh_st)
 {
     int nb_inhib = 0;
     int alias_index = -1;
     char *real_command = command;
 
-    if (config)
+    if (config && config->is_alias_call == false)
         alias_index = find_alias(command, config->alias);
     if (alias_index >= 0)
-        real_command = set_real_command(command, config, alias_index);
-    search_inhib(real_command);
+        return handle_alias(real_command, command, sh_st, alias_index);
+    nb_inhib = search_inhib(real_command);
     if (nb_inhib == 0)
         return str_to_word_array(real_command, " \n\t");
     if (nb_inhib % 2 != 0) {
@@ -94,8 +108,11 @@ static char **format_command(char *command, config_rc_t *config)
 
 char **handle_command(sh_t *sh_st)
 {
-    char **command_array = format_command(sh_st->command, sh_st->config);
+    char **command_array = format_command(sh_st->command, sh_st->config,
+        sh_st);
 
+    if (command_array == NULL)
+        return NULL;
     handle_env_var_call(command_array, *sh_st->envp);
     return command_array;
 }
