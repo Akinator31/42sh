2efaadbd1c65aaee8d6e92b041c3dd276087ccab
Kevin SALANIER <kevin.salanier@epitech.eu>
2025-05-06 08:54:41 +0200

    fix(errno_manager.c): remove exec_return parameter from my_strerror function to improve code readability and maintainability

--- src/utils/errno_manager.c
+++ src/utils/errno_manager.c
@@ -13,12 +13,10 @@
 #include <errno.h>
 #include "my_lib.h"
 
-static char *my_strerror(int exec_return)
+static char *my_strerror(void)
 {
     if (errno == ENOEXEC)
         return "Exec format error. Binary file not executable";
-    if (exec_return == 136)
-        return "Floating exception";
     return strerror(errno);
 }
 
@@ -32,7 +30,7 @@ void errno_manager(int exec_return, char **command_element)
         free_2d_array_of_char(command_element);
         exit(EXIT_FAILURE);
     } else if (exec_return != 0) {
-        error_to_string = my_strerror(exec_return);
+        error_to_string = my_strerror();
         write(2, command_element[0], my_strlen(command_element[0]));
         write(2, ": ", 2);
         write(2, error_to_string, my_strlen(error_to_string));
